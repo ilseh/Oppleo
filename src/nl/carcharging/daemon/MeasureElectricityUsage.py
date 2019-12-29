c3aeddaa1cde76d42f9129fd9d9847db45460410
Ilse Haanstra <ilse.haanstra@gmail.com>
2019-12-29 21:14:16 +0100

    Made it more robust.

--- src/nl/carcharging/daemon/MeasureElectricityUsage.py
+++ src/nl/carcharging/daemon/MeasureElectricityUsage.py
@@ -48,9 +48,8 @@ SECONDS_IN_HOUR = 60 * 60
 class MeasureElectricityUsage(Service):
 
     @inject
-    def __init__(self, energyUtil: EnergyUtil):
+    def __init__(self):
         super(MeasureElectricityUsage, self).__init__(PROCESS_NAME, pid_dir=PID_DIR)
-        self.energyUtil = energyUtil
 
     def run(self):
         self.logger.debug('running')
@@ -59,23 +58,29 @@ class MeasureElectricityUsage(Service):
     def create_save_measurement_jobs(self):
         measure_jobname_base = "measuring_%s"
 
-        self.logger.debug('Searching for measurement devices configured in the db')
+        self.logger.info('Searching for measurement devices configured in the db')
         energy_devices = EnergyDeviceModel.get_all()
-        self.logger.debug('Found %d measurement devices' % len(energy_devices))
+        self.logger.info('Found %d measurement devices' % len(energy_devices))
 
         for energy_device in energy_devices:
-            self.logger.debug('Found energy device %s, adding it to scheduler' % energy_device.energy_device_id)
+            self.logger.info('Found energy device %s, adding it to scheduler' % energy_device.energy_device_id)
             scheduler.add_job(id=measure_jobname_base % energy_device.energy_device_id,
-                              func=handle_measurement, args=[EnergyUtil(), energy_device.energy_device_id],
+                              func=try_handle_measurement, args=[EnergyUtil(), energy_device.energy_device_id],
                               trigger="interval", seconds=10)
 
         scheduler.start()
 
     def remove_measurement_jobs(self):
-        self.logger.debug('>>>>>>>>>>>>>>>.stopping scheduled jobs')
+        self.logger.debug('stopping scheduled jobs')
         scheduler.remove_all_jobs()
         scheduler.shutdown()
 
+def try_handle_measurement(energy_util: EnergyUtil, energy_device_id):
+    try:
+        handle_measurement(energy_util, energy_device_id)
+    except Exception as e:
+        logger = logging.getLogger(PROCESS_NAME)
+        logger.error('Could not execute handle_measurement %s' % e)
 
 def handle_measurement(energy_util: EnergyUtil, energy_device_id):
     logger = logging.getLogger(PROCESS_NAME)
@@ -85,13 +90,19 @@ def handle_measurement(energy_util: EnergyUtil, energy_device_id):
     logger.debug('Measurement returned %s' % data)
     device_measurement = EnergyDeviceMeasureModel(data)
 
-    last_save_measurement = EnergyDeviceMeasureModel.get_last_one()
-    logger.debug('Last save measurement values: %s, %s, %s' % (last_save_measurement.id, last_save_measurement.kw_total,
-                                                               last_save_measurement.created_at))
     logger.debug('New measurement values: %s, %s, %s' % (device_measurement.id, device_measurement.kw_total,
                                                          device_measurement.created_at))
 
-    if has_total_kw_increased(last_save_measurement, device_measurement) \
+    last_save_measurement = EnergyDeviceMeasureModel.get_last_one()
+
+    if last_save_measurement is None:
+        logger.info('No saved measurement found, is this the first run for device %s?' % energy_device_id)
+    else:
+        logger.debug('Last save measurement values: %s, %s, %s' % (last_save_measurement.id, last_save_measurement.kw_total,
+                                                                   last_save_measurement.created_at))
+
+
+    if last_save_measurement is None or has_total_kw_increased(last_save_measurement, device_measurement) \
             or is_measurement_older_than_1hour(last_save_measurement, device_measurement):
         logger.debug('Measurement has changed or old one is older than 1 hour, saving it to db')
         save_measurement(device_measurement)
