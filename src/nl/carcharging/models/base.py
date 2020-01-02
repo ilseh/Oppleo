e0a45a293794547b2ca9c96cbc76db0ba100bdf4
Ilse Haanstra <ilse.haanstra@gmail.com>
2020-01-02 22:28:30 +0100

    Added pool_pre_ping in attempt to fix error
    Error:
     measure_electricity_usage -  ThreadPoolExecutor-0_0 - ERROR - Could not execute handle_measurement (psycopg2.errors.AdminShutdown) terminating connection due to administrator command
    server closed the connection unexpectedly
            This probably means the server terminated abnormally
            before or while processing the request.
    
    [SQL: SELECT energy_device.energy_device_id AS energy_device_energy_device_id, energy_device.port_name AS energy_device_port_name, energy_device.slave_address AS energy_device_slave_address
    FROM energy_device
    WHERE energy_device.energy_device_id = %(energy_device_id_1)s
     LIMIT %(param_1)s]
    [parameters: {'energy_device_id_1': 'laadpaal_noord', 'param_1': 1}]
    (Background on this error at: http://sqlalche.me/e/e3q8)

--- src/nl/carcharging/models/base.py
+++ src/nl/carcharging/models/base.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
-engine = create_engine(os.getenv('DATABASE_URL'))
+engine = create_engine(os.getenv('DATABASE_URL'), pool_pre_ping=True)
 
 
 session_factory = sessionmaker(bind=engine)
