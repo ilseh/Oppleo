d5c6f9ff37bc058990ee3732e6327f4ee87d3492
Ilse Haanstra <ilse.haanstra@gmail.com>
2020-01-05 13:36:46 +0100

    https://trello.com/c/rwN9L2ER/2-status-bijhouden
    Adding charge light indication.

--- src/nl/carcharging/daemon/LedLightHandler.py
+++ src/nl/carcharging/daemon/LedLightHandler.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 
+from nl.carcharging.models.EnergyDeviceMeasureModel import EnergyDeviceMeasureModel
 from nl.carcharging.models.SessionModel import SessionModel
 from nl.carcharging.utils.GenericUtil import GenericUtil
 from nl.carcharging.views.SessionView import session_schema
@@ -13,7 +14,7 @@ try:
 except RuntimeError:
     logging.debug('Assuming dev env')
 
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from injector import inject, Injector
 from service import Service
 
@@ -26,8 +27,9 @@ from nl.carcharging.utils.EnergyUtil import EnergyUtil
 PROCESS_NAME = 'rfid_reader'
 LOG_FILE = '/tmp/%s.log' % PROCESS_NAME
 
+MAX_SECONDS_INTERVAL_CHARGING = 20
 
-scheduler = BlockingScheduler()
+scheduler = BackgroundScheduler()
 
 # TODO: make pid_dir configurable
 PID_DIR = '/tmp/'
@@ -41,7 +43,10 @@ class LedLightHandler(Service):
         super(LedLightHandler, self).__init__(PROCESS_NAME, pid_dir=PID_DIR)
         self.ledlighterAvailable = LedLighter(LedLighter.LED_GREEN)
         self.ledlighterReady = LedLighter(LedLighter.LED_RED, LedLighter.LED_GREEN)
+        self.ledlighterCharging = LedLighter(LedLighter.LED_BLUE)
         self.energy_util = energy_util
+        self.current_light = None
+        self.previous_light = None
 
     def run(self):
         # TODO: Just started up. Check if there was a session in progress.
@@ -50,9 +55,13 @@ class LedLightHandler(Service):
         device = GenericUtil.getMeasurementDevice()
         self.logger.info("Starting rfid reader for device %s" % device)
 
-        current_light = self.ledlighterAvailable
-        current_light.on(5)
+        self.current_light = self.ledlighterAvailable
+        self.current_light.on(5)
 
+        scheduler.add_job(id="check_is_charging",
+                          func=self.handle_charging, args=[device],
+                          trigger="interval", seconds=10)
+        scheduler.start()
 
         reader = RfidReader()
         while not self.got_sigterm():
@@ -78,26 +87,58 @@ class LedLightHandler(Service):
                 ser_data = session_schema.dump(latest_session)
 
 
-            current_light.off()
+            self.turn_current_light_off()
 
             # TODO: if open session than toggle session off (and stop the electricity) and set light to available again.
             #  If no session open, we get a new session and set light to ready (and start electricity flow and ligth to charging).
             if start_session:
-                current_light = self.ledlighterReady
+                self.current_light = self.ledlighterReady
             else:
-                current_light = self.ledlighterAvailable
-            current_light.on(5)
+                self.current_light = self.ledlighterAvailable
+            self.current_light.on(5)
 
             time.sleep(2)
         else:
             self.logger.info("Stopping RfidReader")
             self.stop()
 
+    def turn_current_light_off(self):
+        if self.current_light == self.ledlighterCharging:
+            self.current_light.pulse_stop()
+        else:
+            self.current_light.off()
+
     def stop(self, block=False):
         self.ledlighterAvailable.off()
         self.ledlighterAvailable.cleanup()
         self.ledlighterReady.off()
         self.ledlighterAvailable.cleanup()
+
+    def handle_charging(self, device):
+        last_two_measures = EnergyDeviceMeasureModel().get_last_n_saved(device, 2)
+        diff_last_two_measures_saved = last_two_measures[0].created_at - last_two_measures[1].created_at
+        # Get dummy measure to get current datetime which we can use to see if charging is going pn.
+        current_date_time = EnergyDeviceMeasureModel()
+        current_date_time.set({})
+        diff_now_and_last_saved_session = current_date_time.created_at - last_two_measures[0].created_at
+        # It is charging, let blue light pulse.
+        if diff_now_and_last_saved_session.seconds <= MAX_SECONDS_INTERVAL_CHARGING \
+                and diff_last_two_measures_saved.seconds <= MAX_SECONDS_INTERVAL_CHARGING:
+            self.logger.debug("Device is currently charging")
+            if self.current_light != self.ledlighterCharging:
+                self.previous_light = self.current_light
+                self.turn_current_light_off()
+                self.current_light = self.ledlighterCharging
+                self.current_light.pulse()
+                self.logger.debug("Blue light is pulsing to indicate charging")
+        else:
+            # Not charging. If it was charging, set light back to previous light
+            if self.current_light == self.ledlighterCharging:
+                self.logger.debug("Charging is stopped, setting back previous light %s" % self.previous_light)
+                self.turn_current_light_off()
+                self.current_light = self.previous_light
+                self.current_light.on(5)
+
 
 
 def main():
