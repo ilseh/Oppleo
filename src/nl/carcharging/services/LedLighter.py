cb6a3e66c0d78b4a2bba436deb9df32170d7bfbe
Ilse Haanstra <ilse.haanstra@gmail.com>
2020-01-13 00:08:27 +0100

    Bugfixing regarding reverting to previous light.

--- src/nl/carcharging/services/LedLighter.py
+++ src/nl/carcharging/services/LedLighter.py
@@ -24,10 +24,10 @@ class LedLighter(object):
 
     def __init__(self):
 
-        self.ledlightAvailable = LedLight(LedLight.LED_GREEN)
-        self.ledlightReady = LedLight(LedLight.LED_RED, LedLight.LED_GREEN)
-        self.ledlightCharging = LedLight(LedLight.LED_BLUE, pulse=True)
-        self.ledlightError = LedLight(LedLight.LED_RED)
+        self.ledlightAvailable = LedLight(LedLight.LED_GREEN, intensity=LIGHT_INTENSITY_LOW)
+        self.ledlightReady = LedLight(LedLight.LED_RED, LedLight.LED_GREEN, intensity=LIGHT_INTENSITY_LOW)
+        self.ledlightCharging = LedLight(LedLight.LED_BLUE, pulse=True, intensity=LIGHT_INTENSITY_HIGH)
+        self.ledlightError = LedLight(LedLight.LED_RED, intensity=LIGHT_INTENSITY_HIGH)
 
         # self.ledlight_configs = [self.ledlightAvailable, self.ledlightReady, self.ledlightCharging, self.ledlightError]
 
@@ -41,25 +41,32 @@ class LedLighter(object):
         return self.current_light == self.ledlightCharging
 
     def charging(self):
-        self.switch_to_light(self.ledlightCharging, LIGHT_INTENSITY_LOW)
+        self.switch_to_light(self.ledlightCharging)
 
     def available(self):
-        self.switch_to_light(self.ledlightAvailable, LIGHT_INTENSITY_LOW)
+        self.switch_to_light(self.ledlightAvailable)
 
     def ready(self):
-        self.switch_to_light(self.ledlightReady, LIGHT_INTENSITY_LOW)
+        self.switch_to_light(self.ledlightReady)
 
-    def switch_to_light(self, light, intensity):
+    def switch_to_light(self, light):
         self.lock.acquire()
-        self.save_state()
         self.current_light = light
-        self.current_light.on(intensity)
+        self.save_state()
+        self.current_light.on()
         self.lock.release()
 
     def save_state(self):
-        self.previous_light = self.current_light
         if self.previous_light:
+            self.logger.debug('turning previous light off')
             self.previous_light.off()
+            self.logger.debug('previous light is off')
+
+        self.logger.debug("save state, start comparing")
+        if self.previous_light != self.current_light:
+            self.logger.debug("save state, previous light is different from current")
+            self.previous_light = self.current_light
+
 
     def turn_current_light_off(self):
         if self.current_light == self.ledlightCharging:
@@ -69,24 +76,25 @@ class LedLighter(object):
 
     def error(self, duration=None):
         if duration:
-            self.temp_switch_on_thread(self.ledlightError, LIGHT_INTENSITY_HIGH, duration)
+            self.temp_switch_on_thread(self.ledlightError, duration)
         else:
-            self.switch_to_light(self.ledlightError, LIGHT_INTENSITY_HIGH)
+            self.switch_to_light(self.ledlightError)
 
-    def temp_switch_on_thread(self, light, brightness, duration):
-        thread_for_temp_switch_on = threading.Thread(target=self.temp_switch_on, name="Temp_switch_on thread", args=(light, brightness, duration))
+    def temp_switch_on_thread(self, light, duration):
+        thread_for_temp_switch_on = threading.Thread(target=self.temp_switch_on, name="Temp_switch_on thread",
+                                                     args=(light, duration))
         thread_for_temp_switch_on.start()
 
-    def temp_switch_on(self, light, brightness, duration):
+    def temp_switch_on(self, light, duration):
 
         self.lock.acquire()
         self.save_state()
         self.current_light = light
-        self.current_light.on(brightness)
+        self.current_light.on()
         time.sleep(duration)
         self.current_light.off()
         self.current_light = self.previous_light
-        self.current_light.on(self.current_light.brightness)
+        self.current_light.on()
         self.lock.release()
 
     def stop(self):
