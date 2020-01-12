22053bacf27944e9565ffd6e5fbb2e65f907ae32
Frans Laemen <frans@laemen.com>
2020-01-12 23:00:00 +0100

    Session status start + diagnostics in web

---
+++ src/nl/carcharging/models/User.py
@@ -0,0 +1,27 @@
+from nl.carcharging.models import db
+
+
+class User(db.Model):
+    """
+    """
+    __tablename__ = 'users'
+
+    username = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.username
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
