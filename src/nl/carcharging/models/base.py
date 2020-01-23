d2fb98e635942982b092e7f327762c4ebbbe1fa4
Frans Laemen <frans@laemen.com>
2020-01-24 00:50:27 +0100

    Working on sqlalchemy events for db updates

--- src/nl/carcharging/models/base.py
+++ src/nl/carcharging/models/base.py
@@ -3,10 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
-engine = create_engine(os.getenv('DATABASE_URL'), pool_pre_ping=True)
+from config import WebAppConfig
 
+engine = create_engine(os.getenv(WebAppConfig.PARAM_DB_URL), pool_pre_ping=True)
 
 session_factory = sessionmaker(bind=engine)
 DbSession = scoped_session(session_factory)
 
 Base = declarative_base()
+
+WebAppConfig.sqlalchemy_engine = engine
+WebAppConfig.sqlalchemy_session_factory = session_factory
+WebAppConfig.sqlalchemy_session = DbSession
