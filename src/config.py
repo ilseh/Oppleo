0ef63ea31fb17df3f4eb5e27c85d0f45a0ac0950
Frans Laemen <frans@laemen.com>
2020-01-24 19:37:53 +0100

    config to WebAppConfig

--- src/config.py
+++ src/config.py
@@ -26,27 +26,3 @@ app_config = {
     'production': Production,
 }
 
-
-
-# 
-class WebAppConfig(object):
-    login_manager = None
-    socketio = None
-
-    PARAM_ENV = 'CARCHARGING_ENV'
-    PARAM_DB_URL = 'DATABASE_URL'
-    env = {
-        'development': Development,
-        'production': Production,
-    }
-    sqlalchemy_engine = None
-    sqlalchemy_session_factory = None
-    sqlalchemy_session = None
-    PROCESS_NAME = 'PythonProc'
-    LOG_FILE = '/tmp/%s.log' % PROCESS_NAME
-
-    @staticmethod
-    def initLogger(process_name='PythonProc'):
-        WebAppConfig.PROCESS_NAME = process_name
-        WebAppConfig.LOG_FILE = '/tmp/%s.log' % WebAppConfig.PROCESS_NAME
-        Logger.init_log(WebAppConfig.PROCESS_NAME, WebAppConfig.LOG_FILE)
