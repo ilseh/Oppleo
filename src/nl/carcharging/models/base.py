ba1cdb34dfb84112f0c770773b2c492b280cfeb6
Frans Laemen <frans@laemen.com>
2020-01-25 15:40:41 +0100

    Still install...

--- src/nl/carcharging/models/base.py
+++ src/nl/carcharging/models/base.py
@@ -5,6 +5,13 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from nl.carcharging.config.WebAppConfig import WebAppConfig
 
 print(' 2 WebAppConfig.PARAM_DB_URL: %s ' % WebAppConfig.DATABASE_URL)
+print(' 2 ini: %s ' % WebAppConfig.ini_settings)
+
+if (WebAppConfig.ini_settings == None):
+    WebAppConfig.loadConfig()
+
+print(' 3 WebAppConfig.PARAM_DB_URL: %s ' % WebAppConfig.DATABASE_URL)
+print(' 3 ini: %s ' % WebAppConfig.ini_settings)
 
 engine = create_engine(WebAppConfig.DATABASE_URL, pool_pre_ping=True)
 
