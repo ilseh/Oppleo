ed5ad7616c49854510b5bb05b5b42b84301c8e9f
FRans Laemen <frans.laemen@kpn.com>
2020-01-26 18:45:33 +0100

    more install stuff

--- webapp/install.sh
+++ webapp/install.sh
@@ -1,39 +1,11 @@
 # Reaspberry config
-echo "Update config"
-echo "...stopping nginx..."
-sudo systemctl stop nginx
-echo "uWSGI"
-echo "...stopping uWSGI CarChargerWebApp..."
+echo "Stopping CarChargerWebApp if running..."
 sudo systemctl stop CarChargerWebApp
-echo "...installing CarChargerWebApp.service for uWSGI"
+echo "Update config/ installing CarChargerWebApp.service for systemd..."
 sudo cp CarChargerWebApp.service /etc/systemd/system/CarChargerWebApp.service
-echo "...deleting uwsgi directory in /var/run"
-sudo rm -rf /var/run/uwsgi
-echo "...creating uwsgi directory in /var/run for sock file"
-sudo mkdir /var/run/uwsgi
-sudo chmod 777 /var/run/uwsgi
-sudo chown pi:pi /var/run/uwsgi
-echo "...creating socket file in /var/run"
-touch /var/run/uwsgi/CarChargerWebApp.sock
-echo "...for now chmod 777 on the sock file"
-chown pi:pi /var/run/uwsgi/CarChargerWebApp.sock
-chmod 777 /var/run/uwsgi/CarChargerWebApp.sock
-echo "...reloading daemon config"
+echo "Reloading daemon config..."
 sudo systemctl daemon-reload
-echo "...starting CarChargerWebApp uWSGI daemon"
+echo "Starting CarChargerWebApp systemd daemon..."
 sudo systemctl start CarChargerWebApp
 sudo systemctl status CarChargerWebApp
-echo "NGINX"
-RND="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c20)"
-echo "...moving current default nginx config from default to default.${RND}"
-sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.${RND}
-echo "...installing CarChargerWebApp as default available nginx website"
-sudo cp CarChargerWebApp /etc/nginx/sites-available/default
-echo "...set CarChargerWebApp as default active website" 
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-echo "...Testing config..."
-sudo nginx -t
-echo "...restarting nginx"
-sudo systemctl start nginx
-sudo systemctl status nginx
+echo "Done!"
---
+++ webapp/readme.txt
@@ -0,0 +1,3 @@
+
+uwsgi --ini /home/pi/development/RestfulCharging/webapp/CarChargerWebApp.ini --logger file:/tmp/uwsgi.log -H /home/pi/development/RestfulCharging/venv/
+
