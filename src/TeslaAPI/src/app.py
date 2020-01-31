3a3408dc9b7a0c070e19b4c6df3fe07af723125a
Frans Laemen <frans@laemen.com>
2020-01-31 17:27:17 +0100

    Cleanup and install script env dynamic

--- src/TeslaAPI/src/app.py
+++
@@ -1,7 +0,0 @@
-# importing the requests library 
-from teslaapi import TeslaAPI 
-
-teslaapi = TeslaAPI()
-odometer = teslaapi.getOdometer()
-if (odometer is not None):
-    print("Odometer is {}".format(int(odometer)))
--- webapp/install.sh
+++ webapp/install.sh
@@ -1,4 +1,14 @@
 # Reaspberry config
+echo "Install script for the CarChargerWebApp service"
+echo " v0.2 31-01-2020"
+echo " determining the location of the install script..."
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo " install script in dir $DIR..."
+PRJDIR=${DIR%"/webapp"}
+echo " project root $PRJDIR..."
+echo " preparing service file CarChargerWebApp.service from template by updating path..."
+# sed 's?#WORKINGDIR_PLACEHOLDER?'`pwd`'?' < $DIR/CarChargerWebApp.service.template >$DIR/CarChargerWebApp.service
+sed 's?#WORKINGDIR_PLACEHOLDER?'$PRJDIR'?' < $DIR/CarChargerWebApp.service.template >$DIR/CarChargerWebApp.service
 echo "Stopping CarChargerWebApp if running..."
 sudo systemctl stop CarChargerWebApp
 echo "Update config/ installing CarChargerWebApp.service for systemd..."
