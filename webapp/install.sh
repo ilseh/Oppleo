039c19bc4de9e39c06da71a9914595cddcfb1f42
FRans Laemen <frans.laemen@kpn.com>
2020-02-06 00:26:18 +0100

    Added Pi GPIO Daemon to install script

--- webapp/install.sh
+++ webapp/install.sh
@@ -1,6 +1,59 @@
 # Reaspberry config
 echo "Install script for the CarChargerWebApp service"
 echo "v0.2 31-01-2020"
+
+# Some systemd commands
+# 1. systemd version
+#   systemd --version
+# 2. Check if systemd is runnign or not
+#   ps -eaf | grep [s]ystemd
+# 3. Analyze systemd boot process
+#   systemd-analyze
+# 4. Analyze time taken by each process at boot
+#   systemd --version
+
+# Check prereq: is Pi GPIO Daemon (pigpiod) installed and running? 
+echo "Checking prereqs: is Pi GPIO Daemon (pigpiod) installed and running?"
+IFS=' ' # space is set as delimiter
+read -ra pigpiodSysdEntry <<< $(systemctl list-unit-files --type=service | grep pigpiod)
+if [ "${#pigpiodSysdEntry[@]}" -gt 0 ]; then
+  echo "  found pigpiod, it is "${pigpiodSysdEntry[1]}
+  if [ ${pigpiodSysdEntry[1]} !=  "enabled" ]; then
+    echo "  enabling Pi GPIO Daemon (pigpiod)..."
+    sudo systemctl enable pigpiod.service
+    echo "  starting Pi GPIO Daemon (pigpiod)..."
+    sudo systemctl start pigpiod.service
+    if (!(systemctl -q is-active pigpiod.service)); then
+      echo "  Pi GPIO Daemon (pigpiod) not running."
+      systemctl list-unit-files --type=service | grep pigpiod
+      echo "Install failed"
+      exit 1
+    fi
+  fi
+else
+  # Pi GPIO Daemon is apparently not installed
+  echo "  installing Pi GPIO Daemon..."
+  # Remove old versions
+  rm /home/pi/pigpio.zip
+  sudo rm -rf /home/pi/PIGPIO
+  # Download 
+  wget abyz.me.uk/rpi/pigpio/pigpio.zip -P /home/pi/
+  unzip /home/pi/pigpio.zip
+  cwd=$(pwd)
+  cd /home/pi/PIGPIO
+  make
+  sudo make install
+  # Return to current working directory
+  cd $(cwd)
+  echo "  Pi GPIO Daemon installed."
+  echo "  enabling Pi GPIO Daemon (pigpiod)..."
+  sudo systemctl enable pigpiod.service
+  echo "  starting Pi GPIO Daemon (pigpiod)..."
+  sudo systemctl start pigpiod.service
+fi
+echo "  done checking pigpiod prereq."
+
+echo "Installing the WebApp systemd service..."
 echo " determining the location of the install script..."
 # DIR holds the directory of webapp
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
