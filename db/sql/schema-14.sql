98ce0bcd40389470776f686d18d3a25591832786
Frans Laemen <frans@laemen.com>
2020-02-09 17:09:32 +0100

    db update

--- db/sql/schema-14.sql
+++ db/sql/schema-14.sql
@@ -1,20 +1,19 @@
-alter table "energy_device" ADD "baudrate" int;
-alter table "energy_device" ADD "bytesize" int;
-
-alter table "energy_device" ADD "parity" char(1);
-alter table "energy_device" ADD "stopbits" int;
-alter table "energy_device" ADD "timeout_value" int;
-alter table "energy_device" ADD "debug" boolean;
-alter table "energy_device" ADD "mode" varchar(10);
+alter table energy_device ADD column baudrate int;
+alter table energy_device ADD column bytesize int;
+alter table energy_device ADD column parity char(1);
+alter table energy_device ADD column stopbits int;
+alter table energy_device ADD column timeout int;
+alter table energy_device ADD column debug boolean;
+alter table energy_device ADD column mode varchar(10);
 
 update energy_device 
         set baudrate = 9600, 
             bytesize = 8, 
             parity = 'N', 
             stopbits=1, 
-            timeout_value=1, 
+            timeout=1, 
             debug='t', 
-            mode='rtu',
+            mode='rtu'
         where energy_device_id = 'laadpaal_noord';
 
 
