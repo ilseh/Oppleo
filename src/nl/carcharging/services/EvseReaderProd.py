a6fad164e5e00bc1cd72706a3ea2797fef62e165
Frans Laemen <frans@laemen.com>
2020-02-12 23:43:12 +0100

    Added error filter (3) for EVSE PWM reading

--- src/nl/carcharging/services/EvseReaderProd.py
+++ src/nl/carcharging/services/EvseReaderProd.py
@@ -94,7 +94,7 @@ class EvseReaderProd:
 
     def __init__(self):
         self.logger = logging.getLogger(LOGGER_PATH)
-#        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.WARNING)
 
     def loop(self, cb_until, cb_result):
         global WebAppConfig
