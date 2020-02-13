baed75b02eb312e6ffde55b3a0b274443ed5845a
Laemen, Frans <frans.laemen@kpn.com>
2020-02-13 17:02:49 +0100

    Off-peak model

--- src/nl/carcharging/models/OffPeakHoursModel.py
+++ src/nl/carcharging/models/OffPeakHoursModel.py
@@ -5,7 +5,7 @@ from marshmallow import fields, Schema
 from marshmallow.fields import Boolean
 
 from . import db
-from sqlalchemy import orm
+from sqlalchemy import orm, and_, or_, cast, Time
 from nl.carcharging.models.base import Base, DbSession
 
 
@@ -17,7 +17,10 @@ class OffPeakHoursModel(Base):
 
     id = db.Column(db.Integer, primary_key=True)
     weekday = db.Column(db.String(20))
-    holiday = db.Column(db.Date)
+    holiday_day = db.Column(db.Integer)
+    holiday_month = db.Column(db.Integer)
+    holiday_year = db.Column(db.Integer)
+    recurring = db.Column(db.Boolean)
     description = db.Column(db.String(100))
     off_peak_start = db.Column(db.Time)
     off_peak_end = db.Column(db.Time)
@@ -56,17 +59,30 @@ class OffPeakHoursModel(Base):
         # Weekday?
         r = db_session.query(OffPeakHoursModel) \
                 .filter(OffPeakHoursModel.weekday == self.weekdayToStr(timestamp.weekday())) \
-                .filter(OffPeakHoursModel.off_peak_start <= timestamp.strftime("%H:%M")) \
-                .filter(OffPeakHoursModel.off_peak_end >= timestamp.strftime("%H:%M"))
+                .filter(OffPeakHoursModel.off_peak_start <= cast(timestamp, Time)) \
+                .filter(OffPeakHoursModel.off_peak_end >= cast(timestamp, Time))
         if r is not None and len(r) > 0:
             self.logger.debug('DayOfWeek within off-peak')
             return True
 
         # Is this a public holiday?
         r = db_session.query(OffPeakHoursModel) \
-                .filter(OffPeakHoursModel.holiday == timestamp.strftime("%d/%m/%Y")) \
-                .filter(OffPeakHoursModel.off_peak_start <= timestamp.strftime("%H:%M")) \
-                .filter(OffPeakHoursModel.off_peak_end >= timestamp.strftime("%H:%M"))
+                .filter(
+                    or_(
+                        and_(   # Specific holiday
+                            OffPeakHoursModel.holiday_day == int(timestamp.date().day),
+                            OffPeakHoursModel.holiday_month == int(timestamp.date().month),
+                            OffPeakHoursModel.holiday_year == int(timestamp.date().year)
+                        ),
+                        and_(   # Recurring holiday
+                            OffPeakHoursModel.holiday_day == int(timestamp.date().day),
+                            OffPeakHoursModel.holiday_month == int(timestamp.date().month),
+                            OffPeakHoursModel.recurring == True
+                        )
+                    )
+                    ) \
+                .filter(OffPeakHoursModel.off_peak_start <= cast(timestamp, Time)) \
+                .filter(OffPeakHoursModel.off_peak_end >= cast(timestamp, Time))
 
         if r is not None and len(r) > 0:
             self.logger.debug('Holiday within off-peak')
@@ -82,7 +98,10 @@ class OffPeakHoursModel(Base):
         return ({
                 "id": str(self.id),
                 "weekday": (str(self.weekday) if self.weekday is not None else None),
-                "holiday": (str(self.holiday) if self.holiday is not None else None),
+                "holiday": ((str(self.holiday_day) + '-' + str(self.holiday_month) + '-' + str(self.holiday_year)) \
+                             if self.holiday_day is not None and self.holiday_month is not None and self.holiday_year is not None \
+                             else None),
+                "recurring": str(self.recurring),
                 "description": str(self.description),
                 "off_peak_start": (str(self.off_peak_start) if self.off_peak_start is not None else None),
                 "off_peak_end": (str(self.off_peak_end) if self.off_peak_end is not None else None)
@@ -95,7 +114,10 @@ class ChargerConfigSchema(Schema):
     """
     id = fields.Int(dump_only=True)
     weekday = fields.Str(dump_only=True)
-    holiday = fields.Date(dump_only=True)
+    holiday_day = fields.Int(dump_only=True)
+    holiday_month = fields.Int(dump_only=True)
+    holiday_year = fields.Int(dump_only=True)
+    recurring = fields.Bool(dump_only=True)
     description = fields.Str(dump_only=True)
     off_peak_start = fields.Time(dump_only=True)
     off_peak_end = fields.Time(dump_only=True)
