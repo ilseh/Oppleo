22eeccb04eb5578dd32d150a1525976fa4909d32
Laemen, Frans <frans.laemen@kpn.com>
2020-06-25 23:13:26 +0200

    nodbus usb port and register settings, SDM120 registers added - debug

--- src/nl/oppleo/utils/modbus/SDM120.py
+++ src/nl/oppleo/utils/modbus/SDM120.py
@@ -15,36 +15,36 @@ SDM120 = {
             # 30013 Phase 1 power. [W]
             "P": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 12,   # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 12,   # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30001     0   0  Phase 1 line to neutral volts. [V]
             "V": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 0,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 0,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30007 Phase 1 current. [A]
             "A": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 6,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 6,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             "kWh": {
                 "enabled"   : False,
             }
         },
 
-    "L2"            : {
+        "L2": {
             "P": {
                 "enabled"   : False
             },
@@ -59,7 +59,7 @@ SDM120 = {
             }
         },
 
-        "L3"            : {
+        "L3": {
             "P": {
                 "enabled"   : False
             },
@@ -76,20 +76,22 @@ SDM120 = {
 
         # 30071 Frequency of supply voltages. [Hz]
         "Hz": {
-            "type"  : "float",
-            "ra"    : 70,   # registeraddress
-            "fc"    : 4,    # functioncode,
-            "nor"   : 2,    # number_of_registers
-            "bo"    : 0     # byteorder
+            "enabled"   : True,
+            "type"      : "float",
+            "ra"        : 70,   # registeraddress
+            "fc"        : 4,    # functioncode,
+            "nor"       : 2,    # number_of_registers
+            "bo"        : 0     # byteorder
         },
 
         # 30343   1   56   Total kWh. [kWh]
         "total_kWh": {
-            "type"  : "float",
-            "ra"    : 342,  # registeraddress
-            "fc"    : 4,    # functioncode,
-            "nor"   : 2,    # number_of_registers
-            "bo"    : 0     # byteorder
+            "enabled"   : True,
+            "type"      : "float",
+            "ra"        : 342,  # registeraddress
+            "fc"        : 4,    # functioncode,
+            "nor"       : 2,    # number_of_registers
+            "bo"        : 0     # byteorder
         }
 
     }
--- src/nl/oppleo/utils/modbus/SDM360v2.py
+++ src/nl/oppleo/utils/modbus/SDM360v2.py
@@ -27,138 +27,140 @@ SDM630v2 = {
         },
 
         # read_float(registeraddress, functioncode, number_of_registers, byteorder)
-        "L1"            : {
+        "L1": {
             # 30013 Phase 1 power. [W]
             "P": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 12,   # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 12,   # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30001     0   0  Phase 1 line to neutral volts. [V]
             "V": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 0,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 0,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30007 Phase 1 current. [A]
             "A": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 6,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 6,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30359 Phase 1 total kWh. [kWh]
             "kWh": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 358,  # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 358,  # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             }
         },
 
-    "L2"            : {
+    "L2": {
             # 30015 Phase 2 power. [W]
             "P": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 14,   # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 14,   # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30003     0   2  Phase 2 line to neutral volts. [V]
             "V": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 2,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 2,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30009 Phase 2 current. [A]
             "A": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 8,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 8,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30361 Phase 2 total kWh. [kWh]
             "kWh": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 360,  # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 360,  # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             }
         },
 
-        "L3"            : {
+        "L3": {
             # 30017 Phase 3 power. [W]
             "P": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 16,   # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 16,   # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30005     0   4  Phase 3 line to neutral volts. [V]
             "V": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 4,    # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 4,    # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30011 Phase 3 current. [A]
             "A": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 10,   # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 10,   # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             },
             # 30363 Phase 3 total kWh. [kWh]
             "kWh": {
                 "enabled"   : True,
-                "type"  : "float",
-                "ra"    : 362,  # registeraddress
-                "fc"    : 4,    # functioncode,
-                "nor"   : 2,    # number_of_registers
-                "bo"    : 0     # byteorder
+                "type"      : "float",
+                "ra"        : 362,  # registeraddress
+                "fc"        : 4,    # functioncode,
+                "nor"       : 2,    # number_of_registers
+                "bo"        : 0     # byteorder
             }
         },
 
         # 30071 Frequency of supply voltages. [Hz]
         "Hz": {
-            "type"  : "float",
-            "ra"    : 70,   # registeraddress
-            "fc"    : 4,    # functioncode,
-            "nor"   : 2,    # number_of_registers
-            "bo"    : 0     # byteorder
+            "enabled"   : True,
+            "type"      : "float",
+            "ra"        : 70,   # registeraddress
+            "fc"        : 4,    # functioncode,
+            "nor"       : 2,    # number_of_registers
+            "bo"        : 0     # byteorder
         },
 
         # 30343   1   56   Total kWh. [kWh]
         "total_kWh": {
-            "type"  : "float",
-            "ra"    : 342,  # registeraddress
-            "fc"    : 4,    # functioncode,
-            "nor"   : 2,    # number_of_registers
-            "bo"    : 0     # byteorder
+            "enabled"   : True,
+            "type"      : "float",
+            "ra"        : 342,  # registeraddress
+            "fc"        : 4,    # functioncode,
+            "nor"       : 2,    # number_of_registers
+            "bo"        : 0     # byteorder
         }
     }
