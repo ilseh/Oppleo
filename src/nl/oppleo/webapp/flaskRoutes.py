da2a243b884d5ddf7cbdbc348775df94a9bd1e3c
Laemen, Frans <frans.laemen@kpn.com>
2020-08-06 09:55:39 +0200

    os.popen to os.spawnpl for update

--- src/nl/oppleo/webapp/flaskRoutes.py
+++ src/nl/oppleo/webapp/flaskRoutes.py
@@ -428,7 +428,8 @@ def software_update():
             updateSoftwareLogFile = os.path.join(os.path.dirname(os.path.realpath(__file__)).split('src/nl/oppleo/webapp')[0], 'install/log/update_{}.log'.format(datetime.now().strftime("%Y%m%d%H%M%S")))
             # os.system("nohup sudo -b bash -c 'sleep 2; /home/pi/Oppleo/install/install.sh' &>/dev/null")
             flaskRoutesLogger.debug("nohup sudo -u pi -b bash -c 'sleep 2; {} &> {}'".format(updateSoftwareInstallCmd, updateSoftwareLogFile))
-            os.popen("nohup sudo -u pi -b bash -c 'sleep 2; {} &> {}'".format(updateSoftwareInstallCmd, updateSoftwareLogFile))
+            # update script kills Oppleo, and also os.system or os.popen processes. Spawn new process that will survive the Oppleo kill
+            os.spawnlp(os.P_NOWAIT, "sudo", "-u pi -b bash -c 'sleep 2; {} &> {}'".format(updateSoftwareInstallCmd, updateSoftwareLogFile))
         except Exception as e:
             flaskRoutesLogger.error("Exception running software update! {}".format(e))
         return render_template("softwareupdate.html", 
