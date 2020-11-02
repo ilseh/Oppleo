75cd067b232692e5b8e1646e5830b76b68284145
Laemen, Frans <frans.laemen@kpn.com>
2020-11-02 17:39:57 +0100

    debug

--- src/nl/oppleo/models/Raspberry.py
+++ src/nl/oppleo/models/Raspberry.py
@@ -192,8 +192,8 @@ class Raspberry(object):
 
 
     def getPid(self) -> str:
-        self.logger.debug("getPid(): ".format(str(os.getpid())))
-        self.logger.debug("getPPid(): ".format(str(os.getppid())))
+        self.logger.debug("getPid(): {}".format(str(os.getpid())))
+        self.logger.debug("getPPid(): {}".format(str(os.getppid())))
 
         return str(os.getpid())
 
