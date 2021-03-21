e4baaf96244208b0d0405382ca4c6ed35d03f172
Frans Laemen <frans@laemen.com>
2021-03-21 21:28:33 +0100

    test file fix

--- test/Rfid_read.py
+++ test/Rfid_read.py
@@ -3,15 +3,28 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
+import random, time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+
 reader = SimpleMFRC522()
 
-print ("ready to read...")
+print ("RFID Blocking Read checker (stop using CRTL-C)")
+cnt = 0
 
-try:
-        id, text = reader.read()
-        print("ID:", id)
-        print("Text:", text)
-finally:
-        GPIO.cleanup()
+run = True
+while run:
+        cnt = cnt +1
+        try:
+                print (" calling SimpleMFRC522().read() [{}]".format(cnt))
+                id, text = reader.read()
+                print("  ID:", id)
+                print("  Text:", text)
 
+        except KeyboardInterrupt:
+                run = False
 
+print("Done.")
