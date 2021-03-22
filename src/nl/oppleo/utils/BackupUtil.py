9044a7a50c6e3c323b1eab3f8ef291354b1878f3
Laemen, Frans <frans.laemen@kpn.com>
2021-03-22 17:10:44 +0100

    test file

--- src/nl/oppleo/utils/BackupUtil.py
+++ src/nl/oppleo/utils/BackupUtil.py
@@ -892,7 +892,7 @@ class BackupUtil(object, metaclass=Singleton):
 
     def isBackupDue(self) -> bool:
         lastBackup = self.oppleoConfig.backupSuccessTimestamp
-        self.logger.info('lastBackup: {}'.format(str(lastBackup)))
+        # self.logger.info('lastBackup: {}'.format(str(lastBackup)))
         now = datetime.now()
 
         """
@@ -924,8 +924,8 @@ class BackupUtil(object, metaclass=Singleton):
                              )
                   )
 
-            self.logger.info('Backup due: {} != 8 and {} < {} and {} > {}'.format(
-                daysPast, str(lastBackup), due, now, due))
+            #self.logger.info('Backup due: {} != 8 and {} < {} and {} > {}'.format(
+            #    daysPast, str(lastBackup), due, now, due))
 
             # Backup due? (daysPast=8 indicates no active days)
             return daysPast != 8 and lastBackup < due and now > due
